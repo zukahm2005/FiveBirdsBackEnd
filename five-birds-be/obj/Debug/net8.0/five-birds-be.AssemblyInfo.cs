2cf8e7975a3b116e568e4b38b0d7137a929cbdb0
Lương Tuấn Phi <hpq833592@gmail.com>
2024-11-19 15:55:42 +0700

    a

--- five-birds-be/obj/Debug/net8.0/five-birds-be.AssemblyInfo.cs
+++ five-birds-be/obj/Debug/net8.0/five-birds-be.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("five-birds-be")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21f9b6685ac60ce0bb59683e1acee56e3eae04ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("five-birds-be")]
 [assembly: System.Reflection.AssemblyTitleAttribute("five-birds-be")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
