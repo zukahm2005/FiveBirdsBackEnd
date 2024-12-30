7a3bde67a06bb7bd0d800886645e7ccfbe0c0422
mtuanvu <143334278+mtuanvu@users.noreply.github.com>
2024-12-30 12:05:58 +0700

    update program.cs

--- Program.cs
+++ Program.cs
@@ -156,7 +156,7 @@ app.UseSwaggerUI(c =>
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Documentation V1");
     c.RoutePrefix = string.Empty;
 });
-// app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
 app.UseCookiePolicy(new CookiePolicyOptions
 {
