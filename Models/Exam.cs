963e59103ea8d84ab17ffac3b0524cb8c36903bd
mtuanvu <143334278+mtuanvu@users.noreply.github.com>
2025-01-08 13:37:43 +0700

    exam fields candidate position

--- Models/Exam.cs
+++ Models/Exam.cs
@@ -9,7 +9,7 @@ namespace five_birds_be.Models
         public List<Question> Question { get; set; } = new List<Question>();
         public List<CandidateTest> CandidateTests { get; set; } = new List<CandidateTest>();
 
-         public int CandidatePositionId { get; set; }
+         public int? CandidatePositionId { get; set; }
         public CandidatePosition CandidatePosition { get; set; }
 
     }
